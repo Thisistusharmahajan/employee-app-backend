62e64990ba2ba129c55e61887575d6353e048709
Thisistusharmahajan <96428951+Thisistusharmahajan@users.noreply.github.com>
2025-07-15 16:22:42 +0530

    Update Cors.php
    cors.php modified

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -10,7 +10,7 @@ class Cors implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        header('Access-Control-Allow-Origin: http://localhost:8080'); // ✅ Allow your frontend origin
+        header('Access-Control-Allow-Origin: http://localhost:8080/*'); // ✅ Allow your frontend origin
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
